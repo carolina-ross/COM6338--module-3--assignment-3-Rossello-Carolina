ff3baffb36f0b1d3dc6c05e57181b1c41d2d6ff0
carolina-ross <97426898+carolina-ross@users.noreply.github.com>
2022-06-05 22:47:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+var questionsArr = [
+    {
+    question: 'The Big Apple is a nickname given to San Francisco',  //0
+    answer: false
+    },
+
+    {
+    question: 'Penguins cannot fly',  //1
+    answer: true
+    },
+
+    {
+    question: 'There are more ancient pyramids in Sudan than in Egypt',  //2
+    answer: true
+    },
+
+    {
+    question: 'Ethereum is the second-largest cryptocurrency after Bitcoin',  //3
+    answer: true
+    },
+
+    {
+    question: 'There are three rivers in Saudi Arabia',  //4
+    answer: false
+    }
+]
+
+function runQuiz() {
+    for(var i = 0; i < questionsArr.length; i++) {
+        var question = questionsArr[i]
+        var answer = confirm(question)
+      } 
+}
